882d1201d3516dd131edb241f44d7bb115a22fb0
Matyas Heber <hebertmatyas@gmail.com>
2025-12-28 12:24:21 +0100

    DAY 2 - finally this have a correct name, kinda forgot what was done here but some basics surely

---
+++ game_state.cpp
@@ -0,0 +1,31 @@
+#include "game_state.hpp"
+
+GameState::GameState(){
+    variables.emplace(
+        "x",
+        Variable("x", 0.0, getScoreParams(100, 0, Polarity::Normal))
+    );
+    variables.emplace(
+        "y",
+        Variable("y", 5.0, getScoreParams(100, 0, Polarity::Normal))
+    );
+}
+
+double GameState::getScore(){
+    double score = 0;
+    for (auto& [name, var] : this->variables){
+        score += var.getScore();
+    }
+    return score;
+}
+
+Variable* GameState::getVar(std::string name){
+    auto pair = this->variables.find(name); 
+    if (pair != this->variables.end()){
+        return &(pair->second);
+    }
+    else{
+        return nullptr;
+    }
+    
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include "variable.hpp"
+#include "game_state.hpp"
+#include "expression.hpp"
 
 int main(){
     std::cout << "Henlo" << std::endl;
     Variable var("x", VariableValue(100), {100, 0, Polarity::Normal});
     var.add(-50);
     std::cout << var.getScore() << std::endl;
+
+    GameState gameState;
+    gameState.getVar("x")->add(12);
+    std::cout << gameState.getScore() << std::endl;
+
+    Expression x("x");
+    Expression y("y");
+    Expression expr(&x, Operand::Divide, &y);
+    std::cout << expr.evaluate(&gameState).getAsDouble() << std::endl;
+
     return 0;
 }
